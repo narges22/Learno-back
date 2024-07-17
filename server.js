87701e6569705505a78f1c07eae59703c96b9359
narges22 <narges_shaker@ymail.com>
2024-07-17 09:25:25 +0100

    Fix: port issue

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import http from 'http';
-import _ from 'lodash';
+import http from "http";
+import _ from "lodash";
 
 const server = http.createServer((req, res) => {
-    const number= _.random(0,20 )
-    console.log('request mode',number);
+  const number = _.random(0, 20);
+  console.log("request mode", number);
 });
+var port = process.env.PORT || 8080;
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening')
-    
-})
+server.listen(port, "localhost", () => {
+  console.log("listening");
+});
